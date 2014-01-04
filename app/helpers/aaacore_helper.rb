b3b99ca49bef84d80a9aa177bf3bf0607b54cd5d
abarradah <ahmed.barradah@gmail.com>
2014-01-04 16:46:01 +0000

    first project version

---
+++ app/helpers/aaacore_helper.rb
@@ -0,0 +1,11 @@
+module AaacoreHelper
+  # Return a title on a per-page basis
+  def title
+    base_title ="My Sweet Web Site"
+    if @title.nil?
+      base_title 
+    else
+      "#{base_title}|#{@title}"
+    end 
+  end 
+end
